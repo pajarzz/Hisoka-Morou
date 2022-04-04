95c3509e410d70df9676a4b766f0e5df46a5552b
pajarzz <102685671+pajarzz@users.noreply.github.com>
2022-04-04 14:04:30 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -1,3 +1,9 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
 require('./config')
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@adiwajshing/baileys')
 const fs = require('fs')
